3b7066b38b4ff1cabfe0601df9551f10702ab017
Luca Mincato <lucamincato@gmail.com>
2024-01-24 21:50:58 +0100

    checking if some value already exist (don't work)

--- main.py
+++ main.py
@@ -9,11 +9,31 @@ cur = conn.cursor()
 
 list_name = [('Luca', 'Mincato'), ('Emanuele', 'Mincato'), ('Romano', 'Mincato')]
 
-# cur.execute('''CREATE TABLE users (first name TEXT, last name TEXT)''')
-# conn.commit
+#cur.execute('CREATE TABLE partecipanti1 (name text, surname text)')
+#conn.commit
 
-cur.executemany('''INSERT INTO users VALUES (?,?)''', list_name)
-conn.commit
+cur.execute("""SELECT * FROM partecipanti1 """)
+fetch = cur.fetchall()
+conn.commit()
+inputName = input('inserisci il tuo nome')
+inputSurname = input('inserisci il cognonme')
 
-cur.close()
+for row in fetch:
+    nameFetched = row[0]
+    surnameFetched = row[1]
+    if inputName.strip() not in nameFetched and inputSurname.strip() not in surnameFetched:
+        print('checking')
+    else:
+        print('il cognome esiste già')
+        break
+    cur.execute(""" INSERT INTO partecipanti1 VALUES (?, ?)""", [inputName, inputSurname])
+
+conn.commit()
 conn.close()
+
+conn1 = sqlite3.connect('users.db')
+curs1 = conn1.cursor()
+curs1.execute("""SELECT * FROM partecipanti1 """)
+print(curs1.fetchall())
+conn1.commit()
+conn1.close()
