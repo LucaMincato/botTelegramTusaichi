481c130b0efe64b73577473f6a117d30fe91cc88
Luca Mincato <lucamincato@gmail.com>
2024-01-24 18:16:54 +0100

    populate db

--- main.py
+++ main.py
@@ -5,5 +5,15 @@ import sqlite3
 # connecting to a database
 
 conn = sqlite3.connect('users.db')
+cur = conn.cursor()
 
+list_name = [('Luca', 'Mincato'), ('Emanuele', 'Mincato'), ('Romano', 'Mincato')]
+
+# cur.execute('''CREATE TABLE users (first name TEXT, last name TEXT)''')
+# conn.commit
+
+cur.executemany('''INSERT INTO users VALUES (?,?)''', list_name)
+conn.commit
+
+cur.close()
 conn.close()
