d2cd50fe61d3645c9dbecc617b037388008ae57d
Luca Mincato <lucamincato@gmail.com>
2024-01-26 21:54:58 +0100

    add file

--- main.py
+++ main.py
@@ -10,31 +10,19 @@ from conversation import
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    dictionary = PyDictionary()
-
-
-    application.bot_data['dictionary'] = dictionary
-
-
-    start_handler = CommandHandler('start', start)
-    meaning_handler = CommandHandler('mean', meaning)
-
-
+    startdialog_handler = CommandHandler('start_dialog', start_dialog)
+    start_handler = CommandHandler('start', start) 
+   
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start_dialog',start_dialog)],
         states={
-            ANSWER: [MessageHandler(filters.Regex('(Yes)$'),answer)],
-            ANSWER2: [MessageHandler(filters.Regex('(Yes|no)$'),answer)],
-            ANSWER3: [MessageHandler(filters.Regex('(Yes)$'),answer)],
-            ANSWER4: [MessageHandler(filters.Regex('(Yes)$'),answer)],
-            ANSWER5: [MessageHandler(filters.Regex('(Yes)$'),answer)],
+            ANSWER: [MessageHandler(filters.TEXT,answer),
+                     CommandHandler('cancel',cancel)],
+
         },
         fallbacks=[CommandHandler('cancel',cancel)],
     )
 
     application.add_handler(start_handler)
-    application.add_handler(meaning_handler)
-    application.add_handler(conv_handler)
-
-
+    application.add_handler(startdialog_handler)
     application.run_polling()
