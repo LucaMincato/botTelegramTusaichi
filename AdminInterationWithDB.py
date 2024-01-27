aa5a1bf15d8c1cf5fbbcc6d7d17dd9214206bbda
Luca Mincato <lucamincato@gmail.com>
2024-01-27 18:30:30 +0100

    update debug function

--- AdminInterationWithDB.py
+++ AdminInterationWithDB.py
@@ -1,16 +1,24 @@
 import sqlite3
 
-# conn = sqlite3.connect('telegramBot.db')
-# cur = conn.cursor()
-# 
-# cur.execute('CREATE TABLE partecipanti2 (username text, chatId int)')
-# conn.commit
+#conn = sqlite3.connect('telegramBot.db')
+#cur = conn.cursor()
+#
+#cur.execute('CREATE TABLE partecipanti2 (username text, chatId int)')
+#conn.commit
 
 conn1 = sqlite3.connect('telegramBot.db')
 curs1 = conn1.cursor()
-curs1.execute("""SELECT chatId FROM partecipanti2 """)
-print(curs1.fetchall())
+fetchChatId = curs1.execute("""SELECT chatId FROM partecipanti2 """)
+print(fetchChatId)
+
+dummy = []
+
+for row in fetchChatId:
+    i = 0
+    dummy.append(row[0])
+    i = i + 1
+
 conn1.commit()
 conn1.close()
 
-
+print(dummy)
