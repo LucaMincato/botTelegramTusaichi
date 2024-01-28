8062c2a68e3fca766d98cf9e04525effe59e5b9d
Luca Mincato <lucamincato@gmail.com>
2024-01-28 17:06:09 +0100

    add command that only Luca can use

--- command.py
+++ command.py
@@ -13,6 +13,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 async def getPartecipant(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        chat_id = update.effective_chat.id
+        is_it_admin = CheckAddmin(chat_id)
         users = getUserQuery()
-        await context.bot.send_message(chat_id = 6307311132, text=(f'{users}'), parse_mode='HTML')
-
+        if is_it_admin:
+            await context.bot.send_message(chat_id = 6307311132, text=('Username, chatId, Squadra, tuSaiChi'), parse_mode='HTML')
+            for row in users:
+                await context.bot.send_message(chat_id = 6307311132, text=(f'{row}\n'), parse_mode='HTML')
+        else:
+            await context.bot.send_message(chat_id=chat_id, text='Mi dispiace ma solo Luca Può usare questo comando', parse_mode='HTML')
+   
