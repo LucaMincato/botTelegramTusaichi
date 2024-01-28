41e4e788e77251946bb7bcb1d5f1074c1bca191d
Luca Mincato <lucamincato@gmail.com>
2024-01-28 17:05:20 +0100

    Add new command conversation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from telegram.ext import ConversationHandler
 
 from command import start, getPartecipant
 from conversation import start_dialog, answer, cancel, ANSWER
+from conversation import startAdminInsertPartecipant, teamAdminInsertPartecipant, tuSaiChiAdminInsertPartecipant, endAdminInsertPartecipant, NOME, SQUADRA, TUSAICHI
 from SecretToken import BOT_TOKEN
 
 if __name__ == '__main__':
@@ -12,10 +13,10 @@ if __name__ == '__main__':
 
    # startdialog_handler = CommandHandler('start_dialog', start_dialog)
     start_handler = CommandHandler('start', start) 
-    list_handler = CommandHandler('getPartecipant', getPartecipant) 
+    list_handler = CommandHandler('getpartecipant', getPartecipant) 
    
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start_dialog',start_dialog)],
+        entry_points=[CommandHandler('addme',start_dialog)],
         states={
             ANSWER: [MessageHandler(filters.TEXT,answer),
                      CommandHandler('cancel',cancel)],
@@ -24,10 +25,23 @@ if __name__ == '__main__':
         fallbacks=[CommandHandler('cancel',cancel)],
     )
 
+    conv1_handler = ConversationHandler(
+    entry_points=[CommandHandler('addpartecipant',startAdminInsertPartecipant)],
+    states={
+        NOME: [MessageHandler(filters.TEXT,teamAdminInsertPartecipant),
+                 CommandHandler('cancel',cancel)],
+        SQUADRA: [MessageHandler(filters.TEXT,tuSaiChiAdminInsertPartecipant),
+                 CommandHandler('cancel',cancel)],
+        TUSAICHI: [MessageHandler(filters.TEXT,endAdminInsertPartecipant),
+                 CommandHandler('cancel',cancel)],
+    },
+        fallbacks=[CommandHandler('cancel',cancel)],
+    )
+
     application.add_handler(start_handler)
     application.add_handler(conv_handler)
     application.add_handler(list_handler)
-
+    application.add_handler(conv1_handler)
 
     application.run_polling()
     
